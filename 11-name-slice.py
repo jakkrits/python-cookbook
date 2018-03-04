0c7d233e7224ab5de47cf62ddee62dad9983ba50
JakkritS <jakk.sutt@gmail.com>
2018-03-04 23:50:39 +0700

    update readme

--- 11-name-slice.py
+++ 11-name-slice.py
@@ -1,3 +1,26 @@
 items = [0, 1, 2, 3, 4, 5, 6]
 a = slice(2, 4)
 print(items[a])
+
+items[a] = [111, 222]
+print(items)
+
+del items[a]
+print(items)
+
+a = slice(2, 10, 3)
+print(a.start)
+print(a.stop)
+print(a.step)
+
+print('*' * 20)
+
+# map a slice onto a sequence of a specific size by using its indi
+# ces(size) method. This returns a tuple(start, stop, step)
+s = 'helloWORLD'
+a.indices(len(s))
+for i in range(*a.indices(len(s))):
+    print(s[i])
+
+print(a.indices(len(s)))
+print(*a.indices(len(s)))
